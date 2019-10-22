f86354674365b7e80c3dcff35dd4afdaa122dff2
Son1812 <thesunday007@gmail.com>
2019-10-22 19:12:32 +0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<h2>Son</h2>'; 
+	echo'<img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/10/20/18/Marcus-Rashford.jpg?w968h681" alt="">'
+
+
+  ?> 
+ </body>
+</html>
